6941da5705505296e2cc09c91630be73bf914006
Madison Ofarril <mofarril.mof@gmail.com>
2019-10-14 21:29:41 -0400

    basic file structure

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+-- Create the database day_planner_db and specified it for use.
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+-- Create the table plans.
+CREATE TABLE burger (
+  id int NOT NULL AUTO_INCREMENT,
+  burger varchar(255) NOT NULL,
+  PRIMARY KEY (id)
+);
---
+++ server.js
@@ -0,0 +1,29 @@
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "PasswordRoot",
+  database: "burgers_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+});
