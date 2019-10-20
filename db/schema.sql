6d62868c67b8343fb7c9dc8035e5002ed5ba9963
Madison Ofarril <mofarril.mof@gmail.com>
2019-10-20 18:13:45 -0400

    updated the sql files

--- db/schema.sql
+++ db/schema.sql
@@ -8,6 +8,7 @@ USE burgers_db;
 -- Create the table plans.
 CREATE TABLE burger (
   id int NOT NULL AUTO_INCREMENT,
-  burger varchar(255) NOT NULL,
+  burger_name varchar(255) NOT NULL,
+  devoured BOOLEAN DEFAULT false,
   PRIMARY KEY (id)
 );
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burger (burger_name, devoured) VALUES ('Scottwitch', true);
+INSERT INTO burger(burger_name, devoured) VALUES ('Hallelujah Burger', true);
+INSERT INTO burger(burger_name, devoured) VALUES ('Mercutio Burger', true);
