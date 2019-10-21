d3d2626ef77141e5bdcafd03fb5082f1f6fe0836
Madison Ofarril <mofarril.mof@gmail.com>
2019-10-20 20:33:46 -0400

    set up handlebars

---
+++ controlers/burgers_controller.js
@@ -0,0 +1,14 @@
+var express = require("express");
+// Import the model (burger.js) to use its database functions.
+var burger = require("../models/burger.js");
+//creates router
+var router = express.Router();
+
+
+
+
+
+
+
+// Export routes for server.js to use.
+module.exports = router;
